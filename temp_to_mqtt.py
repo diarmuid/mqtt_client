745779d5c3e339c1dc8942afb3a4cf821f0d9421
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2019-10-04 20:47:57 +0100

    added 1 wire

--- temp_to_mqtt.py
+++ temp_to_mqtt.py
@@ -5,6 +5,7 @@ import adafruit_mcp9808
 import adafruit_bmp280
 import time
 import sys
+from ds18b20 import DS18B20
 
 
 broker_address="192.168.1.201"
@@ -22,6 +23,12 @@ except Exception as e:
 
 bmp280.mode = adafruit_bmp280.MODE_NORMAL
 
+try:
+    ext_sensor = DS18B20()
+except:
+    ext_sensor_en = False
+else:
+    ext_sensor_en = True
 
 #broker_address="iot.eclipse.org" #use external broker
 client = mqtt.Client("P1") #create new instance
@@ -38,6 +45,10 @@ while True:
         print(".", end='')
         client.publish("home/house/temperature", temp)
         client.publish("home/house/pressure", pressure)
+        if ext_sensor_en:
+            ext_temp = ext_sensor.get_temperature()
+            client.publish("home/house/outsidetemperature", ext_temp)
+
     except:
         pass
     time.sleep(1)
