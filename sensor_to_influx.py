c71cfa2b5248796056bbeff84089b7f11593b45d
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2019-10-22 18:25:29 +0100

    Moved to remove mqtt middleman and instead write direct to the influx db

---
+++ sensor_to_influx.py
@@ -0,0 +1,110 @@
+import board
+import busio
+import adafruit_mcp9808
+import adafruit_bmp280
+import time
+import sys
+from ds18b20 import DS18B20
+from influxdb import InfluxDBClient
+from typing import NamedTuple
+
+# Influx database details
+INFLUXDB_ADDRESS = 'ec2-52-17-10-22.eu-west-1.compute.amazonaws.com'
+INFLUXDB_USER = 'root'
+INFLUXDB_PASSWORD = 'root'
+INFLUXDB_DATABASE = 'home_db'
+
+
+class SensorData(NamedTuple):
+    location: str
+    measurement: str
+    value: float
+
+
+def _init_influxdb_database(influxdb_client):
+    """
+    Connect to an influx database
+    :param influxdb_client:
+    :return:
+    """
+    databases = influxdb_client.get_list_database()
+    print("Connected to influx database")
+    if len(list(filter(lambda x: x['name'] == INFLUXDB_DATABASE, databases))) == 0:
+        influxdb_client.create_database(INFLUXDB_DATABASE)
+    influxdb_client.switch_database(INFLUXDB_DATABASE)
+
+
+def send_sensor_data_to_influxdb(sensor_data, influxdb_client):
+    """
+    Send the data to the client
+    :param sensor_data:
+    :param influxdb_client:
+    :return:
+    """
+    json_body = [
+        {
+            'measurement': sensor_data.measurement,
+            'tags': {
+                'location': sensor_data.location
+            },
+            'fields': {
+                'value': sensor_data.value
+            }
+        }
+    ]
+    if not influxdb_client.ping():
+        _init_influxdb_database(influxdb_client)
+
+    influxdb_client.write_points(json_body)
+    #print("Sending data to influx {}".format(json_body))
+
+
+# I2c of indoor pressure and temp sensor
+
+i2c = busio.I2C(board.SCL, board.SDA)
+try:
+    bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+except Exception as e:
+    sys.stderr.write("Can't read sensor. Error={}".format(e))
+    sys.exit(1)
+
+bmp280.mode = adafruit_bmp280.MODE_NORMAL
+# The outside temperature sensor
+try:
+    ext_sensor = DS18B20()
+except:
+    ext_sensor_en = False
+else:
+    ext_sensor_en = True
+
+influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
+_init_influxdb_database(influxdb_client)
+
+while True:
+    #
+    try:
+        temp = bmp280.temperature
+        pressure = bmp280.pressure
+    except:
+        pass
+    else:
+        data = SensorData("house", "temperature", temp)
+        send_sensor_data_to_influxdb(data, influxdb_client)
+
+    try:
+        pressure = bmp280.pressure
+    except:
+        pass
+    else:
+        data = SensorData("house", "pressure", pressure)
+        send_sensor_data_to_influxdb(data, influxdb_client)
+    if ext_sensor_en:
+        try:
+            ext_temp = ext_sensor.get_temperature()
+        except:
+            pass
+        else:
+            data = SensorData("house", "outsidetemperature", ext_temp)
+            send_sensor_data_to_influxdb(data, influxdb_client)
+
+    time.sleep(1)
--- tests/read_1wire.py
+++ tests/read_1wire.py
@@ -1,6 +1,8 @@
 from ds18b20 import DS18B20
 import time
 
+print(DS18B20.get_available_sensors())
+
 sensor = DS18B20()
 while True:
     temperature_in_celsius = sensor.get_temperature()
